a8db4769df4245450c31ed08af8776e0d5821da2
Elena Iliina <elena.iliina@gmail.com>
2026-03-08 16:53:19 +0200

    Add Variables and Comments

---
+++ src/main/java/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,59 @@
+package variablesandcomments;
+/**
+ * Thie is a comment for a line of a code
+ *
+ *
+ *
+ * */
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+        //Declare Variables and comments//
+
+
+        boolean trueBoolean;
+        boolean falseBoolean;
+        char firstAscii;
+        char lastAscii;
+        int lowerIntLimit;
+        int upperIntLimit;
+        double loweDoublelimit;
+        double upperDoubleLimit;
+        byte lowerByteLimit;
+        byte upperByteLimit;
+        long lowerLongLimit;
+        long upperLongLimit;
+        float lowerFloatLimit;
+        float upperFloatLimit;
+        short lowerShortLimit;
+        short upperShortLimit;
+
+        /*this is a comment for the n
+        * lines which are displayed
+        * here */
+
+
+/**
+ * Initialisation of the
+ * variables*/
+
+trueBoolean =true;
+falseBoolean=false;
+  firstAscii = 'a';
+  lastAscii='z';
+  lowerByteLimit = -128;
+  upperByteLimit = 127;
+  lowerIntLimit = -2147483648;
+  upperIntLimit= 2147483647;
+  lowerFloatLimit=-3.4E+38f;
+  upperFloatLimit= 3.4E+38f;
+  lowerLongLimit= -9223372036854775808L;
+  upperLongLimit= 9223372036854775807L;
+  lowerShortLimit= -32768;
+  upperShortLimit=32767;
+  loweDoublelimit=-1.7E+308;
+  upperDoubleLimit=1.7E+308;
+
+
+    }
+}
