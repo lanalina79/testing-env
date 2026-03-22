4e57bd5367b782cf22bc084e865c46868994594c
Elena Iliina <elena.iliina@gmail.com>
2026-03-22 15:10:47 +0200

    Add full implementation for Person and ChisinauEvidenceService

--- src/main/java/accessmodifyers/ChisinauEvidenceService.java
+++ src/main/java/accessmodifyers/ChisinauEvidenceService.java
@@ -1,4 +1,28 @@
 package accessmodifyers;
 
+import accessmodifyers.peopleevidence.Person;
+
+import java.util.Arrays;
+
 public class ChisinauEvidenceService {
+    public static void main(String[] args) {
+
+        Person ionel=new Person("Ionel", "Istrati", 34,73975495l, false, 'M' );
+        Person marcel=new Person();
+
+        System.out.println(ionel.getSex() + ionel.getIdnp()+ ionel.name+ionel.surname+ionel.age+ionel.getIsRetired());
+
+        ionel.age=23;
+        ionel.name="ionel";
+        ionel.surname="Spataru";
+
+        System.out.println(Person.nationality);
+
+        Person.nationality = "Moldavian Edited";
+
+        System.out.println(Person.nationality);
+
+
+
+    }
 }
